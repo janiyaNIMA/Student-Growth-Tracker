e2d7157027e1b79239b30647faac6c9de1c87870
janiyaNIMA <114753817+janiyaNIMA@users.noreply.github.com>
2025-12-06 11:38:26 +0530

    Configure SQLAlchemy settings in app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,8 @@ HERE = os.path.dirname(__file__)
 template_dir = os.path.abspath(os.path.join(HERE, '..', 'frontend'))
 static_dir = os.path.abspath(os.path.join(HERE, '..', 'frontend', 'static'))
 
-app.con
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
